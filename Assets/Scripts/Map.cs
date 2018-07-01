143b7179af55e5cc05183aaed401d424cb71dec6
SibaDoge <rladus753159@gmail.com>
2018-07-02 05:20:24 +0900

    매커니즘 대부분 구현
    뭔가 하다보니 대부분 구현해버림...
    1. 돌 생성, 내려오기, 버튼이동, 회전, 정지, 모이면 삭제, 게임끝 판별 등등

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Map : MonoBehaviour
+{
+    public int Width; //가로 칸수
+    public int Height;//세로 칸수
+    public int successH;//쌓아야하는 칸(0부터 시작)
+    public GameObject[,] map;
+    private Text txt;
+    // Use this for initialization
+    void Awake()
+    {
+        map = new GameObject[Width, Height];
+        txt = GameObject.Find("Text (1)").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        printMap();
+    }
+    public Vector2 roundVec2(Vector2 v)
+    {
+        return new Vector2(Mathf.Round(v.x), Mathf.Round(v.y));
+    }
+
+    public Vector2 rotate(Vector2 vec, float deg)
+    {
+        Vector2 result;
+        result.x = vec.x * Mathf.Cos(Mathf.Deg2Rad * deg) - vec.y * Mathf.Sin(Mathf.Deg2Rad * deg);
+        result.y = vec.x * Mathf.Sin(Mathf.Deg2Rad * deg) + vec.y * Mathf.Cos(Mathf.Deg2Rad * deg);
+        return result;
+    }
+
+    public bool isInside(Vector2 pos)
+    {
+        return ((int)pos.x >= 0 && (int)pos.x < Width && (int)pos.y >= 0);
+    }
+
+    public void updateStone(GameObject obj)
+    {
+        // Remove old children from grid
+        for (int y = 0; y < Height; ++y)
+            for (int x = 0; x < Width; ++x)
+                if (map[x, y] != null)
+                    if (map[x, y].transform == obj.transform)
+                        map[x, y] = null;
+        Vector2 v = roundVec2(obj.transform.position);
+        map[(int)v.x, (int)v.y] = obj;
+        obj.GetComponent<Stone>().mapPos = new Vector2((int)v.x, (int)v.y);
+    }
+
+    public void deleteStone(GameObject obj)
+    {
+        for (int y = 0; y < Height; ++y)
+            for (int x = 0; x < Width; ++x)
+                if (map[x, y] != null)
+                    if (map[x, y].transform == obj.transform)
+                        map[x, y] = null;
+    }
+
+    public void printMap() {
+        txt.text = "";
+        for (int j = Height-1; j > -1; j--)
+        {
+            for (int i = 0; i < Width; i++)
+            {
+                if (map[i, j] == null) txt.text += "0 ";
+                else txt.text += "1 ";
+            }
+            txt.text += "\n";
+        }
+    }
+}
--- Assets/Scripts/StoneSet.cs
+++ Assets/Scripts/StoneSet.cs
@@ -2,15 +2,145 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StoneSet : MonoBehaviour {
+public class StoneSet : MonoBehaviour
+{
+    private Transform[] children;
+    private GameManager GM;
+    private Map mp;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+
+    void Awake()
+    {
+    }
+
+    void Start()
+    {
+        children = gameObject.GetComponentsInChildren<Transform>();
+        mp = GameObject.Find("Main Camera").GetComponent<Map>();
+        GM = GameObject.Find("Main Camera").GetComponent<GameManager>();
+        updateMap();
+        StartCoroutine("fall");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isStuckedSet())
+        {
+            Debug.Log("stop");
+            stopMove();
+        }
+    }
+
+    public void clicked(string str)
+    {
+        switch (str)
+        {
+            case "left": Move("left"); break;
+            case "right": Move("right"); break;
+            case "down": Move("down"); break;
+            case "up":
+                transform.Rotate(0, 0, -90);
+                if (isValidPosSet())
+                    updateMap();
+                else
+                    transform.Rotate(0, 0, 90);
+                break;
+        }
+    }
+
+    public bool isValidPosSet()
+    {
+        foreach (Transform child in children)
+        {
+            if (child.gameObject == gameObject)
+                continue;
+            if (!(child.gameObject.GetComponent<Stone>().isValidPos()))
+                return false; 
+        }
+        return true;
+    }
+    public bool isStuckedSet()
+    {
+        foreach (Transform child in children)
+        {
+            if (child.gameObject == gameObject)
+                continue;
+            if ((child.gameObject.GetComponent<Stone>().isStucked()))
+                return true;
+        }
+        return false;
+    }
+
+    public void Move(string dir)
+    {
+        Vector2 vec = Vector2.zero;
+        switch (dir)
+        {
+            case "left": vec = Vector2.left; break;
+            case "right": vec = Vector2.right; break;
+            case "down": vec = Vector2.down; break;
+        }
+        transform.position += (Vector3)vec;
+        if (isValidPosSet())
+        {
+            updateMap();
+        }
+        else
+        {
+            Debug.Log("not");
+            transform.position -= (Vector3)vec;
+        }
+        posRoundSet();
+    }
+
+    public void posRoundSet()
+    {
+        Vector2 origin = mp.roundVec2(transform.position);
+        transform.position = origin;
+        foreach (Transform child in children)
+        {
+            if (child.gameObject == gameObject)
+                continue;
+            child.gameObject.GetComponent<Stone>().posRound();
+        }
+    }
+
+    public void updateMap()
+    {
+        foreach (Transform child in children)
+        {
+            if (child.gameObject == gameObject)
+                continue;
+            mp.updateStone(child.gameObject);
+        }
+    }
+
+    public void stopMove()
+    {
+        StopCoroutine("fall");
+        foreach (Transform child in children)
+        {
+            if (child.gameObject == gameObject)
+                continue;
+            child.gameObject.GetComponent<Stone>().startMove();
+            child.parent = null;
+        }
+        GM.doNext();
+        Destroy(gameObject);
+    }
+
+
+    IEnumerator fall()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(GM.fallDelay);
+            Debug.Log("move");
+            Move("down");
+
+        }
+    }
+
 }
